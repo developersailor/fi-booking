69874aae25f3a360055475f25aca40f6d3869335
mehmet fiskindal <mehmetfiskindal@gmail.com>
2024-06-15 14:25:42 +0300

    chore: Update .gitignore and package.json for backend changes

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,14 +1,8 @@
+// models/booking.js
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
--- backend/models/user.js
+++ backend/models/user.js
@@ -17,22 +17,3 @@ module.exports = (sequelize, DataTypes) => {
   return User;
 };
 
-// models/booking.js
-'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Booking extends Model {
-    static associate(models) {
-      // define association here
-    }
-  }
-  Booking.init({
-    username: DataTypes.STRING,
-    checkInDate: DataTypes.DATE,
-    checkOutDate: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Booking',
-  });
-  return Booking;
-};
